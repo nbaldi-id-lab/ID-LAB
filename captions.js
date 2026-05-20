473bb0bd4095b6cec4cc03dad2f3a2934dd74684
nbaldi-id-lab <nbaldi@id-lab.nyc>
2026-05-20 18:35:09 -0400

    Update captions.js

--- captions.js
+++ captions.js
@@ -8,13 +8,19 @@ const imageCaptions = {
     "06.jpg": "OMA · Louis Vuitton Visionary Journeys – Fashion Icons, Shanghai 2025",
     "06-B.jpg": "OMA · Louis Vuitton Visionary Journeys – LV & Japan, Osaka 2025",
     "07.jpg": "OMA · Louis Vuitton Visionary Journeys – Music, Seoul 2025",
+       "07-B.jpg": "OMA · Louis Vuitton Visionary Journeys – Music, Seoul 2025",
+
     "08.jpg": "Anastasio Architects & Stefano Pasqualetti Design - The Boston Consulting Group (BCG), Miami 2020",
     "09.jpg": "Anastasio Architects & Stefano Pasqualetti Design - The Boston Consulting Group (BCG), Miami 2020",
-    "10.jpg": "Anastasio Architects & Stefano Pasqualetti Design - The Boston Consulting Group (BCG), Miami 2020",    "11.jpg": "Anastasio Architects · A. Pasta Bar interior design by Alessandro Pasquale Studio · New York · 2017",
+    "10.jpg": "Anastasio Architects & Stefano Pasqualetti Design - The Boston Consulting Group (BCG), Miami 2020",    
+   "10-B.jpg": "Anastasio Architects & Stefano Pasqualetti Design - The Boston Consulting Group (BCG), Miami 2020",   
+   "11.jpg": "Anastasio Architects · A. Pasta Bar interior design by Alessandro Pasquale Studio · New York · 2017",
     "12.jpg": "Anastasio Architects · A. Pasta Bar interior design by Alessandro DiPasquale Studio, New York, 2017",
     "13.jpg": "AR Architecture · Dear Irving on Hudson interior design by Delphine Mauroit, New York, 2018",
     "14.jpg": "AR Architecture · Dear Irving on Hudson interior design by Delphine Mauroit, New York, 2018",
     "15.jpg": "Anastasio Architects · Giada Flagship concept by Claudio Silvestrin, Boston, 2019",
+       "15-B.jpg": "Anastasio Architects · Giada Flagship concept by Claudio Silvestrin, Boston, 2019",
+
     "16.jpg": "Anastasio Architects · Giada Flagship concept by Claudio Silvestrin, Boston, 2019",
     "17.jpg": "Anastasio Architects · Golden Goose, Chicago, 2019",
     "18.jpg": "Anastasio Architects · Golden Goose, Boston, 2020",
@@ -22,4 +28,6 @@ const imageCaptions = {
     "20.jpg": "OMA · Louis Vuitton Visionary Journeys – LV & Japan, Osaka, 2025",
     "21.jpg": "OMA · Louis Vuitton Visionary Journeys – Atelier, Osaka, 2025",
     "22.jpg": "OMA · Louis Vuitton Visionary Journeys – Materials, Osaka, 2025"
+   "23.jpg": "OMA · Louis Vuitton Visionary Journeys – Books, Shanghai 2025",
+
 };
