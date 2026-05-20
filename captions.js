14589318c99b8b1515e5d58aa83d2677aede8bd7
nbaldi-id-lab <nbaldi@id-lab.nyc>
2026-05-20 18:12:58 -0400

    Create captions.js

---
+++ captions.js
@@ -0,0 +1,26 @@
+// captions.js
+const imageCaptions = {
+    "01.jpg": "OMA · Louis Vuitton Visionary Journeys – Origins<br>Osaka · 2025",
+    "02.jpg": "OMA · Louis Vuitton Visionary Journeys – Monogram<br>Osaka · 2025",
+    "03.jpg": "OMA · Louis Vuitton Visionary Journeys – Trunkscape<br>Bangkok · 2024",
+    "04.jpg": "OMA · Louis Vuitton Visionary Journeys – Giveaway<br>Bangkok · 2024",
+    "05.jpg": "OMA · Louis Vuitton Visionary Journeys – Sport<br>Shanghai · 2025",
+    "06.jpg": "OMA · Louis Vuitton Visionary Journeys – Fashion Icons<br>Shanghai · 2025",
+    "06-B.jpg": "OMA · Louis Vuitton Visionary Journeys – LV & Japan<br>Osaka · 2025",
+    "07.jpg": "OMA · Louis Vuitton Visionary Journeys – Music<br>Seoul · 2025",
+    "08.jpg": "Anastasio Architects & Stefano Pasqualetti Design<br>The Boston Consulting Group (BCG) · Miami · 2020",
+    "09.jpg": "Anastasio Architects & Stefano Pasqualetti Design<br>The Boston Consulting Group (BCG) · Miami · 2020",
+    "10.jpg": "Anastasio Architects & Stefano Pasqualetti Design<br>The Boston Consulting Group (BCG) · Miami · 2020",
+    "11.jpg": "Anastasio Architects · A. Pasta Bar<br>interior design by Alessandro Pasquale Studio · New York · 2017",
+    "12.jpg": "Anastasio Architects · A. Pasta Bar<br>interior design by Alessandro Pasquale Studio · New York · 2017",
+    "13.jpg": "AR Architecture · Dear Irving on Hudson<br>interior design by Delphine Mauroit · New York · 2018",
+    "14.jpg": "AR Architecture · Dear Irving on Hudson<br>interior design by Delphine Mauroit · New York · 2018",
+    "15.jpg": "Anastasio Architects · Giada Flagship<br>concept by Claudio Silvestrin · Boston · 2019",
+    "16.jpg": "Anastasio Architects · Giada Flagship<br>concept by Claudio Silvestrin · Boston · 2019",
+    "17.jpg": "Anastasio Architects · Golden Goose<br>Chicago · 2019",
+    "18.jpg": "Anastasio Architects · Golden Goose<br>Boston · 2020",
+    "19.jpg": "OMA · Louis Vuitton Visionary Journeys – Trunkscape<br>Osaka · 2025",
+    "20.jpg": "OMA · Louis Vuitton Visionary Journeys – LV & Japan<br>Osaka · 2025",
+    "21.jpg": "OMA · Louis Vuitton Visionary Journeys – Atelier<br>Osaka · 2025",
+    "22.jpg": "OMA · Louis Vuitton Visionary Journeys – Materials<br>Osaka · 2025"
+};
